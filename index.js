16fdc638e832b6aa37c80d4de7bea53056ac44e7
hasibul240 <hasibul112002@gmail.com>
2022-12-21 13:17:33 +0600

    sharping

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//capturra_lansational
-//J6bUBYwYjmtgAsH2
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3njemyu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -113,8 +110,7 @@ const run = async () => {
         });
 
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 
@@ -123,7 +119,6 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
-
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
